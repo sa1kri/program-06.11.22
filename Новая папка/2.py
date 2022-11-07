69d4b3397c51c147dc7b3cc646a15c0a789958d1
sa1kri <92201506+sa1kri@users.noreply.github.com>
2022-11-07 18:45:55 +0500

    Add files via upload

--- Новая папка/2.py
+++ Новая папка/2.py
@@ -1,15 +1,17 @@
+i=[]
 a=[1,2,3,4]
 b=['s','f','g']
-c=[11,22,'ee']
-a.extend(b)
-a.extend(c)
-print(a)
+i.extend(a)
+i.extend(b)
+print(i)
 
-mylist = []
-for x in range(max(len(a), len(b))):
-    if x < len(a):
-        mylist.append(a[x])
-    if x < len(b):
-        mylist.append(b[x])
+def up():
+    mylist = []
+    for x in range(max(len(a), len(b),)):
+        if x < len(a):
+            mylist.append(a[x])
+        if x < len(b):
+            mylist.append(b[x])
         
-    print(mylist)
+        print(mylist)
+up()
